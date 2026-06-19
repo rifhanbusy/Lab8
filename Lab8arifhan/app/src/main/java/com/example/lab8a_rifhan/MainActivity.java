27be580dfe9b1567b2e578cd63181f33a044958d
rifhan <119849760+rifhanbusy@users.noreply.github.com>
2026-06-19 18:23:42 +0800

    Add files via upload

---
+++ Lab8arifhan/app/src/main/java/com/example/lab8a_rifhan/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.lab8a_rifhan;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    Button openMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        openMap = (Button) findViewById(R.id.button);
+    }
+
+    public void openMap(View view) {
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+        startActivity(intent);
+    }
+}
